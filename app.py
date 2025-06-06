46fbdc6ce5f396cc4976ee413715f4b2344d98c8
YU TAI PAN <pan@2441-pan.local>
2025-06-06 15:28:35 +0900

    password2

--- app.py
+++ app.py
@@ -219,5 +219,4 @@ def update_timeseries(station_ids, selected_case):
     return children
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8050))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=10000)
